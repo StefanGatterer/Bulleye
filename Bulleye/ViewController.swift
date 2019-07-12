510186281f6720f8fe2105c3ae35d1726ae61eb9
Stefan Gatterer <stefan.gatterer@gmail.com>
2019-07-12 14:03:34 +0200

    interface and connections

--- Bulleye/ViewController.swift
+++ Bulleye/ViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    var randomValue =  0
+    @IBOutlet weak var valueLabel: UILabel!
+    var currentValue = 0
+    @IBOutlet weak var roundLabel: UILabel!
+    var round = 1
+    @IBOutlet weak var scoreLabel: UILabel!
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateLabels()
+        
+    }
+    
+    func updateLabels() {
+        roundLabel.text = String(round)
+        scoreLabel.text = String(score)
+        valueLabel.text = String(currentValue)
     }
 
-
+    @IBAction func hitMe() {
+        print("Hit Me")
+        newRound()
+    }
+    
+    @IBAction func restart() {
+        print("restart")
+    }
+    
+    @IBAction func info() {
+        print("info")
+        
+    }
+    
+    func startGame() {
+    
+    }
+    
+    func newRound() {
+        valueLabel.text = String(slider.value)
+    }
+    
+    @IBAction func sliderMoved() {
+        print(slider.value)
+        currentValue = Int(slider.value)
+        valueLabel.text = String(currentValue)
+        
+    }
 }
 
